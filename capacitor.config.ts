3b0449cfe92067894e5aacd4b8e14d67d1bd2372
Marlon Harrison <marlon@ionic.io>
2024-05-06 12:56:08 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'issue-cap-http-ng-maps',
+  webDir: 'www'
+};
+
+export default config;
